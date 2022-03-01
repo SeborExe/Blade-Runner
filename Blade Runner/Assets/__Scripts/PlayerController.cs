f44e88dc66bfcfb309084e99e7038bd061df2da5
SeborExe <mix98@o2.pl>
2022-03-01 20:00:29 +0100

    Jump
    Jump animation (Blender tree).
    Now possible is jumping more than one time.

--- Blade Runner/Assets/__Scripts/PlayerController.cs
+++ Blade Runner/Assets/__Scripts/PlayerController.cs
@@ -7,26 +7,61 @@ public class PlayerController : MonoBehaviour
     [Header("Definiowane w panelu inspekcyjnym")]
     public float movementSpeed = 8f;
     public float jumpForce = 16f;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public int amoutOfJumps = 1;
 
     [Header("Definiowane dynamicznie")]
     private Rigidbody2D rb;
+    private Animator anim;
     private float movementInputDirection;
     private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+    private int amoutOfJumpsLeft;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amoutOfJumpsLeft = amoutOfJumps;
     }
 
     void Update()
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0)
+        {
+            amoutOfJumpsLeft = amoutOfJumps;
+        }
+        if (amoutOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
     }
 
     private void CheckMovementDirection()
@@ -39,6 +74,22 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if (rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private void CheckInput()
@@ -53,7 +104,11 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amoutOfJumpsLeft--;
+        }
     }
 
     private void ApplyMovement()
@@ -65,5 +120,10 @@ public class PlayerController : MonoBehaviour
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
