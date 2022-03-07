049f2b90f7e89c173114d84602e47ac612cbd630
SeborExe <mix98@o2.pl>
2022-03-07 09:14:42 +0100

    Enemy1 melee attack
    enemy 1 mele attack state, animations and script has been created. Small refactorization.

--- Blade Runner/Assets/__Scripts/Enemy/State Machine/Entity.cs
+++ Blade Runner/Assets/__Scripts/Enemy/State Machine/Entity.cs
@@ -86,5 +86,9 @@ public class Entity : MonoBehaviour
     {
         Gizmos.DrawLine(wallCheck.position, wallCheck.position + (Vector3)(Vector2.right * facingDirection * entityData.wallCheckDistance));
         Gizmos.DrawLine(ledgeCheck.position, ledgeCheck.position + (Vector3)(Vector2.down * entityData.ledgeCheckDistance));
+
+        Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.closeRangeActionDistance), 0.2f);
+        Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.minAgroDistance), 0.2f);
+        Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.maxAgroDistance), 0.2f);
     }
 }
